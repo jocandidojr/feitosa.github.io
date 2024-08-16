03a4b13fb5954acdc433285387a0e0ab1ee6a0e3
Josenildo Cândido Jr. <129548924+jocandidojr@users.noreply.github.com>
2024-08-16 19:49:24 -0300

    update

--- proxyServer.js
+++ proxyServer.js
@@ -81,7 +81,7 @@ app.post('/proxy/contratos', async (req, res) => {
   }
 });
 
-// Nova Rota para buscar OSS
+// Rota para buscar OSS
 app.post('/proxy/oss', async (req, res) => {
   const { clienteId } = req.body;
 
@@ -119,78 +119,78 @@ app.post('/proxy/oss', async (req, res) => {
   }
 });
 
-// Rota para criar OSS
-app.post('/proxy/criar-oss', async (req, res) => {
+// Nova Rota para criar OSS
+app.post('/proxy/criar-ossx', async (req, res) => {
+  const { clienteId, tipo, id_assunto, id_filial, origem_endereco, prioridade, setor, mensagem, status } = req.body;
+
+  if (!clienteId || !tipo || !id_assunto || !id_filial || !origem_endereco || !prioridade || !setor || !status) {
+    return res.status(400).json({ error: 'Dados obrigatórios não fornecidos' });
+  }
+
   try {
-    const { clienteId } = req.body;
-
-    if (!clienteId) {
-      return res.status(400).json({ error: 'ClienteId não fornecido' });
-    }
-
     console.log('Criando OSS para o Cliente...');
 
     const response = await axios.post(
       'https://feitosatelecom.com.br/webservice/v1/su_oss_chamado',
       {
-        "tipo": "C",
-        "id_ticket": "",
-        "protocolo": "",
-        "id_assunto": "1",
-        "id_cliente": clienteId,
-        "id_estrutura": "",
-        "id_filial": "1",
-        "id_login": "",
-        "id_contrato_kit": "",
-        "origem_endereco": "M",
-        "origem_endereco_estrutura": "",
-        "latitude": "",
-        "longitude": "",
-        "prioridade": "1",
-        "melhor_horario_agenda": "",
-        "setor": "1",
-        "id_tecnico": "",
-        "mensagem": "Chamado de Teste de Rota",
-        "idx": "",
-        "status": "A",
-        "gera_comissao": "",
-        "liberado": "",
-        "impresso": "",
-        "preview": "",
-        "id_wfl_param_os": "",
-        "id_wfl_tarefa": "",
-        "id_su_diagnostico": "",
-        "regiao_manutencao": "",
-        "origem_cadastro": "",
-        "origem_change_endereco": "",
-        "status_sla": "",
-        "ultima_atualizacao": "",
-        "id_cidade": "",
-        "bairro": "",
-        "endereco": "",
-        "complemento": "",
-        "referencia": "",
-        "id_condominio": "",
-        "bloco": "",
-        "apartamento": "",
-        "data_abertura": "",
-        "data_inicio": "",
-        "data_hora_analise": "",
-        "data_agenda": "",
-        "data_agenda_final": "",
-        "data_hora_encaminhado": "",
-        "data_hora_assumido": "",
-        "data_hora_execucao": "",
-        "data_final": "",
-        "data_fechamento": "",
-        "data_prazo_limite": "",
-        "data_reservada": "",
-        "data_reagendar": "",
-        "data_prev_final": "",
-        "mensagem_resposta": "",
-        "justificativa_sla_atrasado": "",
-        "valor_unit_comissao": "",
-        "valor_total_comissao": ""
+        tipo,
+        id_ticket: "",
+        protocolo: "",
+        id_assunto,
+        id_cliente: clienteId,
+        id_estrutura: "",
+        id_filial,
+        id_login: "",
+        id_contrato_kit: "",
+        origem_endereco,
+        origem_endereco_estrutura: "",
+        latitude: "",
+        longitude: "",
+        prioridade,
+        melhor_horario_agenda: "",
+        setor,
+        id_tecnico: "",
+        mensagem,
+        idx: "",
+        status,
+        gera_comissao: "",
+        liberado: "",
+        impresso: "",
+        preview: "",
+        id_wfl_param_os: "",
+        id_wfl_tarefa: "",
+        id_su_diagnostico: "",
+        regiao_manutencao: "",
+        origem_cadastro: "",
+        origem_change_endereco: "",
+        status_sla: "",
+        ultima_atualizacao: "",
+        id_cidade: "",
+        bairro: "",
+        endereco: "",
+        complemento: "",
+        referencia: "",
+        id_condominio: "",
+        bloco: "",
+        apartamento: "",
+        data_abertura: "",
+        data_inicio: "",
+        data_hora_analise: "",
+        data_agenda: "",
+        data_agenda_final: "",
+        data_hora_encaminhado: "",
+        data_hora_assumido: "",
+        data_hora_execucao: "",
+        data_final: "",
+        data_fechamento: "",
+        data_prazo_limite: "",
+        data_reservada: "",
+        data_reagendar: "",
+        data_prev_final: "",
+        mensagem_resposta: "",
+        justificativa_sla_atrasado: "",
+        valor_unit_comissao: "",
+        valor_total_comissao: ""
       },
       {
         headers: {
@@ -202,22 +202,12 @@ app.post('/proxy/criar-oss', async (req, res) => {
     );
 
     res.json(response.data);
-        
   } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error(
-        "Erro ao criar OSS:",
-        error.response ? error.response.data : error.message
-      );
-      res.status(error.response?.status || 500).json(error.response?.data || { error: "Erro ao criar OSS" });
-    } else {
-      console.error("Erro inesperado:", error);
-      res.status(500).json({ error: "Erro inesperado ao criar OSS" });
-    }
+    console.error('Erro ao criar OSS:', error.message);
+    res.status(error.response?.status || 500).json(error.response?.data || { error: "Erro ao conectar com a API de OSS" });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
-
